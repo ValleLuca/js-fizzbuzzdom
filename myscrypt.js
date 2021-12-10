7d52b22faa9a7803d76015c88c7c825fdc6ccafc
ValleLuca <valle.lucaAb@outlook.it>
2021-12-10 17:10:42 +0100

    Contatore in console concluso

--- myscrypt.js
+++ myscrypt.js
@@ -1,21 +1,20 @@
 //inserire un for che conta da 1 a 100
-for(let i = 0; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
 
-    if (i%3 == 0) {
-        console.log('Fizz');
+    if ( (i%3 == 0) && (i%5 == 0) ) {
+        console.log('FizzBuzz');  //multipli sia di 3 che di 5 "FizzBuzz"
+    } 
+    else if  (i%3 == 0) {
+        console.log('Fizz'); //multipli di tre al posto del numero scritta "Fizz"
     } 
     else if (i%5 == 0) {
-        console.log('Buzz');
-    } 
-    else if (i%3 & i%5 == 0) {
-        console.log('FizzBuzz');
+        console.log('Buzz'); //multipli di cinque al posto del numero scritta "Buzz"
     }
     else {
-        console.log('Numero: ', i);
+        console.log('Numero: ', i); //stampare nella console il risultato
     }
 
 }
-//stampare nella console il risultato
-//multipli di tre al posto del numero scritta "Fizz"
-//multipli di cinque al posto del numero scritta "Buzz"
-//multipli sia di 3 che di 5 "FizzBuzz"
+
+
+
