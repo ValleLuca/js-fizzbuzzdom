1ddb89dc3894d5a57f9e6a16966f49af5e30b438
ValleLuca <valle.lucaAb@outlook.it>
2021-12-10 15:44:38 +0100

    Aggiunti commenti su ciò che c'è da fare

---
+++ myscrypt.js
@@ -0,0 +1,5 @@
+//inserire un for che conta da 1 a 100
+//stampare nella console il risultato
+//multipli di tre al posto del numero scritta "Fizz"
+//multipli di cinque al posto del numero scritta "Buzz"
+//multipli sia di 3 che di 5 "FizzBuzz"
