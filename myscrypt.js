8d5aa83df75c589501f129ad00eaa737d36bf526
ValleLuca <valle.lucaAb@outlook.it>
2021-12-10 16:48:56 +0100

    Aggiunta in console numeri e fizz e buzz

--- myscrypt.js
+++ myscrypt.js
@@ -1,4 +1,20 @@
 //inserire un for che conta da 1 a 100
+for(let i = 0; i <= 100; i++){
+
+    if (i%3 == 0) {
+        console.log('Fizz');
+    } 
+    else if (i%5 == 0) {
+        console.log('Buzz');
+    } 
+    else if (i%3 & i%5 == 0) {
+        console.log('FizzBuzz');
+    }
+    else {
+        console.log('Numero: ', i);
+    }
+
+}
 //stampare nella console il risultato
 //multipli di tre al posto del numero scritta "Fizz"
 //multipli di cinque al posto del numero scritta "Buzz"
